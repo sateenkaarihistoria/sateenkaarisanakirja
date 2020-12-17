666d6f1d56e9473eeeffee2f0457d40efc9037bc
sateenkaarihistoria <puheenjohtaja@sateenkaarihistoria.fi>
2020-12-17 13:02:01 +0200

    Fix asiasana update form

--- client/src/components/organisaatioView/TapahtumaPaivitys.jsx
+++ client/src/components/organisaatioView/TapahtumaPaivitys.jsx
@@ -4,44 +4,44 @@ import { Container, Form, Button, Modal, Header } from 'semantic-ui-react';
 // TODO virheen käsittely duplikaatti emailin tapauksissa tehty erittäin rumasti, korjattava?
 // TODO salasanat, vain salasanan "omistaja" saa muuttaa salasanaansa editoinnin yhteydessä?
 export default function TapahtumaPaivitys(props) {
-  const { tapahtuma, updateHandler } = props
-  const [paivitysModaaliAktiivinen, setPaivitysModaaliAktiivinen] = useState(false)
+  const { tapahtuma, updateHandler } = props;
+  const [paivitysModaaliAktiivinen, setPaivitysModaaliAktiivinen] = useState(
+    false,
+  );
   const [lomakeData, setLomakeData] = useState({
     nimi: '',
-    luonne:'',
-    paivays:'',
-    valmis:false,
-    viesti:'',
-    asiasana: ''
+    luonne: '',
+    paivays: '',
+    valmis: false,
+    viesti: '',
+    asiasana: '',
   });
 
   useEffect(() => {
-      //setIsEdit(true);
-      setLomakeData({
-        nimi: tapahtuma.nimi,
-        luonne: tapahtuma.luonne,
-        paivays: tapahtuma.paivays,
-        valmis: tapahtuma.valmis,
-        viesti: tapahtuma.viesti,
-        asiasana: tapahtuma.asiasana[0]
-      })
-  // eslint-disable-next-line
+    //setIsEdit(true);
+    setLomakeData({
+      nimi: tapahtuma.nimi,
+      luonne: tapahtuma.luonne,
+      paivays: tapahtuma.paivays,
+      valmis: tapahtuma.valmis,
+      viesti: tapahtuma.viesti,
+      asiasana: tapahtuma.asiasana.join(';'),
+    });
+    // eslint-disable-next-line
   }, []);
-
 
   const handleChange = (event, result) => {
     const { name, value } = result || event.target;
-    setLomakeData({...lomakeData, [name]: value });
-  }
-  
-  const handleCheckbox = (event, result) => {
-    const { name } = result || event.target
-    setLomakeData( {...lomakeData, [name]: !lomakeData.valmis });
+    setLomakeData({ ...lomakeData, [name]: value });
   };
 
-  const lahetaPaivitettyData = (e) => {
-    if (e)
-      e.preventDefault()
+  const handleCheckbox = (event, result) => {
+    const { name } = result || event.target;
+    setLomakeData({ ...lomakeData, [name]: !lomakeData.valmis });
+  };
+
+  const lahetaPaivitettyData = e => {
+    if (e) e.preventDefault();
     // @body: JSON-objekti: tapahtuma_nimi, luonne, paivays, kuvaus, viesti, valmis
     const uusiData = {
       tapahtuma_nimi: lomakeData.nimi,
@@ -49,85 +49,90 @@ export default function TapahtumaPaivitys(props) {
       paivays: lomakeData.paivays,
       valmis: lomakeData.valmis,
       viesti: lomakeData.viesti,
-      kuvaus: lomakeData.asiasana
-    }
+      kuvaus: lomakeData.asiasana,
+    };
     if (
-      uusiData.tapahtuma_nimi === '' || uusiData.paivays === '' || uusiData.luonne === '' || 
+      uusiData.tapahtuma_nimi === '' ||
+      uusiData.paivays === '' ||
+      uusiData.luonne === '' ||
       uusiData.asiasana === ''
     ) {
-      console.log("Virhe! Ei tyhjiä kenttiä.")
+      console.log('Virhe! Ei tyhjiä kenttiä.');
     } else {
-      updateHandler ({ tyyppi: 'tapahtuma', id: tapahtuma.id }) (uusiData)
-      hallinnoiSulku()
+      updateHandler({ tyyppi: 'tapahtuma', id: tapahtuma.id })(uusiData);
+      hallinnoiSulku();
     }
-  }
+  };
 
-  const hallinnoiAvaus = () => setPaivitysModaaliAktiivinen(true)
+  const hallinnoiAvaus = () => setPaivitysModaaliAktiivinen(true);
 
-  const hallinnoiSulku = (e) => {
-    if (e)
-      e.preventDefault()
-    setPaivitysModaaliAktiivinen(false)
-  }
+  const hallinnoiSulku = e => {
+    if (e) e.preventDefault();
+    setPaivitysModaaliAktiivinen(false);
+  };
 
   return (
     <Modal
       trigger={
-        <Button compact size='mini' onClick={hallinnoiAvaus}>Päivitä</Button>
+        <Button compact size="mini" onClick={hallinnoiAvaus}>
+          Päivitä
+        </Button>
       }
       open={paivitysModaaliAktiivinen}
     >
-      <Container style={{ padding: "2rem" }}>
+      <Container style={{ padding: '2rem' }}>
         <Header>Tapahtuman tietojen päivitys</Header>
         <Form>
           <Form.Group widths="equal">
             <Form.Input
-              label='Tapahtuman nimi'
-              name='nimi'
-              value={ lomakeData.nimi }
-              onChange={ handleChange }
+              label="Tapahtuman nimi"
+              name="nimi"
+              value={lomakeData.nimi}
+              onChange={handleChange}
             />
-          <Form.Input
-              label='Tapahtuman luonne'
-              name='luonne'
-              value={ lomakeData.luonne }
-              onChange={ handleChange }
+            <Form.Input
+              label="Tapahtuman luonne"
+              name="luonne"
+              value={lomakeData.luonne}
+              onChange={handleChange}
             />
           </Form.Group>
           <Form.Group widths="equal">
             <Form.Input
-                label='Tapahtuman uutisointi'
-                name='paivays'
-                value={ lomakeData.paivays }
-                onChange={ handleChange }
-              />
+              label="Tapahtuman uutisointi"
+              name="paivays"
+              value={lomakeData.paivays}
+              onChange={handleChange}
+            />
             <Form.Input
-              label='Asiasana'
-              name='asiasana'
-              value={ lomakeData.asiasana }
-              onChange={ handleChange }
+              label="Asiasana"
+              name="asiasana"
+              value={lomakeData.asiasana}
+              onChange={handleChange}
             />
           </Form.Group>
           <Form.Group widths="equal">
             <Form.Checkbox
-                style={{ marginTop: '1.0rem', marginBottom: '1.0rem' }}
-                label="Tapahtuman syöttö on valmis julkaistavaksi"
-                name="valmis"
-                checked={lomakeData.valmis}
-                onChange={ handleCheckbox }
-              />          
-          </Form.Group>          
+              style={{ marginTop: '1.0rem', marginBottom: '1.0rem' }}
+              label="Tapahtuman syöttö on valmis julkaistavaksi"
+              name="valmis"
+              checked={lomakeData.valmis}
+              onChange={handleCheckbox}
+            />
+          </Form.Group>
           <Form.Group widths="equal">
             <Form.TextArea
               label="Viesti"
               placeholder="Kommentti toiselle tutkijalle tms."
               name="viesti"
               value={lomakeData.viesti}
-              onChange={ handleChange }
+              onChange={handleChange}
             />
           </Form.Group>
-          <Button type="submit" onClick={ lahetaPaivitettyData }>Tallenna muutokset</Button> 
-          <Button onClick={ hallinnoiSulku }>Peruuta</Button>
+          <Button type="submit" onClick={lahetaPaivitettyData}>
+            Tallenna muutokset
+          </Button>
+          <Button onClick={hallinnoiSulku}>Peruuta</Button>
         </Form>
       </Container>
     </Modal>
--- client/src/components/sanakirjaView/IlmentymaPaivitys.jsx
+++ client/src/components/sanakirjaView/IlmentymaPaivitys.jsx
@@ -4,50 +4,50 @@ import { Container, Form, Button, Modal, Header } from 'semantic-ui-react';
 // TODO virheen käsittely duplikaatti emailin tapauksissa tehty erittäin rumasti, korjattava?
 // TODO salasanat, vain salasanan "omistaja" saa muuttaa salasanaansa editoinnin yhteydessä?
 export default function IlmentymaPaivitys(props) {
-  const { ilmentyma, updateHandler } = props
-  const [paivitysModaaliAktiivinen, setPaivitysModaaliAktiivinen] = useState(false)
+  const { ilmentyma, updateHandler } = props;
+  const [paivitysModaaliAktiivinen, setPaivitysModaaliAktiivinen] = useState(
+    false,
+  );
   const [lomakeData, setLomakeData] = useState({
     hs_osio: '',
-    paivays:'',
-    selite:'',
-    tyyli:'',
-    kayttoala:'',
-    lause:'',
-    valmis:false,
-    viesti:'',
-    asiasana: ''
+    paivays: '',
+    selite: '',
+    tyyli: '',
+    kayttoala: '',
+    lause: '',
+    valmis: false,
+    viesti: '',
+    asiasana: '',
   });
 
   useEffect(() => {
-      //setIsEdit(true);
-      setLomakeData({
-        hs_osio: ilmentyma.hs_osio,
-        paivays: ilmentyma.sivunumero,
-        selite: ilmentyma.selite,
-        tyyli: ilmentyma.tyyli,
-        kayttoala: ilmentyma.kayttoala,
-        lause: ilmentyma.lause,
-        valmis: ilmentyma.valmis,
-        viesti: ilmentyma.viesti,
-        asiasana: ilmentyma.asiasana
-      })
-  // eslint-disable-next-line
+    //setIsEdit(true);
+    setLomakeData({
+      hs_osio: ilmentyma.hs_osio,
+      paivays: ilmentyma.sivunumero,
+      selite: ilmentyma.selite,
+      tyyli: ilmentyma.tyyli,
+      kayttoala: ilmentyma.kayttoala,
+      lause: ilmentyma.lause,
+      valmis: ilmentyma.valmis,
+      viesti: ilmentyma.viesti,
+      asiasana: ilmentyma.asiasana.join(';'),
+    });
+    // eslint-disable-next-line
   }, []);
-
 
   const handleChange = (event, result) => {
     const { name, value } = result || event.target;
-    setLomakeData({...lomakeData, [name]: value });
-  }
-    
-  const handleCheckbox = (event, result) => {
-    const { name } = result || event.target
-    setLomakeData( {...lomakeData, [name]: !lomakeData.valmis });
+    setLomakeData({ ...lomakeData, [name]: value });
   };
 
-  const lahetaPaivitettyData = (e) => {
-    if (e)
-      e.preventDefault()
+  const handleCheckbox = (event, result) => {
+    const { name } = result || event.target;
+    setLomakeData({ ...lomakeData, [name]: !lomakeData.valmis });
+  };
+
+  const lahetaPaivitettyData = e => {
+    if (e) e.preventDefault();
     // @body: hs_osio, paivays, selite, tyyli, kayttoala, lause, kuvaus, viesti, valmis
     const uusiData = {
       hs_osio: lomakeData.hs_osio,
@@ -58,108 +58,115 @@ export default function IlmentymaPaivitys(props) {
       lause: lomakeData.lause,
       valmis: lomakeData.valmis,
       viesti: lomakeData.viesti,
-      kuvaus: lomakeData.asiasana
-    }
+      kuvaus: lomakeData.asiasana,
+    };
     if (
-      uusiData.hs_osio === '' || uusiData.paivays === '' || uusiData.selite === '' || 
-      uusiData.tyyli === '' || uusiData.kayttoala === '' || uusiData.lause === '' || 
+      uusiData.hs_osio === '' ||
+      uusiData.paivays === '' ||
+      uusiData.selite === '' ||
+      uusiData.tyyli === '' ||
+      uusiData.kayttoala === '' ||
+      uusiData.lause === '' ||
       uusiData.asiasana === ''
     ) {
-      console.log("Virhe! Ei tyhjiä kenttiä.")
+      console.log('Virhe! Ei tyhjiä kenttiä.');
     } else {
-      updateHandler ({ tyyppi: 'ilmentyma', id: ilmentyma.id }) (uusiData)
-      hallinnoiSulku()
+      updateHandler({ tyyppi: 'ilmentyma', id: ilmentyma.id })(uusiData);
+      hallinnoiSulku();
     }
-  }
+  };
 
-  const hallinnoiAvaus = () => setPaivitysModaaliAktiivinen(true)
+  const hallinnoiAvaus = () => setPaivitysModaaliAktiivinen(true);
 
-  const hallinnoiSulku = (e) => {
-    if (e)
-      e.preventDefault()
-    setPaivitysModaaliAktiivinen(false)
-  }
+  const hallinnoiSulku = e => {
+    if (e) e.preventDefault();
+    setPaivitysModaaliAktiivinen(false);
+  };
 
   return (
     <Modal
       trigger={
-        <Button compact size='mini' onClick={hallinnoiAvaus}>Päivitä</Button>
+        <Button compact size="mini" onClick={hallinnoiAvaus}>
+          Päivitä
+        </Button>
       }
       open={paivitysModaaliAktiivinen}
     >
-    <Container style={{ padding: "2rem" }}>
-      <Header>Ilmentymän tietojen päivitys</Header>
+      <Container style={{ padding: '2rem' }}>
+        <Header>Ilmentymän tietojen päivitys</Header>
         <Form>
           <Form.Group widths="equal">
             <Form.Input
-              label='Päiväys'
-              name='paivays'
-              value={ lomakeData.paivays }
-              onChange={ handleChange }
+              label="Päiväys"
+              name="paivays"
+              value={lomakeData.paivays}
+              onChange={handleChange}
             />
             <Form.Input
-              label='Lehden osio'
-              name='hs_osio'
-              value={ lomakeData.hs_osio }
-              onChange={ handleChange }
-            />
-          </Form.Group>
-          <Form.Group widths="equal">
-            <Form.Input
-              label='Asiasana'
-              name='asiasana'
-              value={ lomakeData.asiasana }
-              onChange={ handleChange }
-            />
-            <Form.Input
-              label='Selite'
-              name='selite'
-              value={ lomakeData.selite }
-              onChange={ handleChange }
+              label="Lehden osio"
+              name="hs_osio"
+              value={lomakeData.hs_osio}
+              onChange={handleChange}
             />
           </Form.Group>
           <Form.Group widths="equal">
             <Form.Input
-              label='Tyyli'
-              name='tyyli'
-              value={ lomakeData.tyyli }
-              onChange={ handleChange }
+              label="Asiasana"
+              name="asiasana"
+              value={lomakeData.asiasana}
+              onChange={handleChange}
             />
             <Form.Input
-              label='Käyttöala'
-              name='kayttoala'
-              value={ lomakeData.kayttoala }
-              onChange={ handleChange }
+              label="Selite"
+              name="selite"
+              value={lomakeData.selite}
+              onChange={handleChange}
             />
           </Form.Group>
           <Form.Group widths="equal">
             <Form.Input
-                label='Lause'
-                name='lause'
-                value={ lomakeData.lause }
-                onChange={ handleChange }
-              />
+              label="Tyyli"
+              name="tyyli"
+              value={lomakeData.tyyli}
+              onChange={handleChange}
+            />
+            <Form.Input
+              label="Käyttöala"
+              name="kayttoala"
+              value={lomakeData.kayttoala}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group widths="equal">
+            <Form.Input
+              label="Lause"
+              name="lause"
+              value={lomakeData.lause}
+              onChange={handleChange}
+            />
           </Form.Group>
           <Form.Group widths="equal">
             <Form.Checkbox
-                style={{ marginTop: '1.0rem', marginBottom: '1.0rem' }}
-                label="Sanan syöttö on valmis julkaistavaksi"
-                name="valmis"
-                checked={lomakeData.valmis}
-                onChange={ handleCheckbox }
-              />          
-          </Form.Group>          
+              style={{ marginTop: '1.0rem', marginBottom: '1.0rem' }}
+              label="Sanan syöttö on valmis julkaistavaksi"
+              name="valmis"
+              checked={lomakeData.valmis}
+              onChange={handleCheckbox}
+            />
+          </Form.Group>
           <Form.Group widths="equal">
             <Form.TextArea
               label="Viesti"
               placeholder="Kommentti toiselle tutkijalle tms."
               name="viesti"
               value={lomakeData.viesti}
-              onChange={ handleChange }
+              onChange={handleChange}
             />
           </Form.Group>
-          <Button type="submit" onClick={ lahetaPaivitettyData }>Tallenna muutokset</Button> 
-          <Button onClick={ hallinnoiSulku }>Peruuta</Button>
+          <Button type="submit" onClick={lahetaPaivitettyData}>
+            Tallenna muutokset
+          </Button>
+          <Button onClick={hallinnoiSulku}>Peruuta</Button>
         </Form>
       </Container>
     </Modal>
