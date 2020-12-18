77e03495c8251dcc8574b2eb6da4951b333b60b0
sateenkaarihistoria <puheenjohtaja@sateenkaarihistoria.fi>
2020-12-18 08:03:51 +0200

    Fix search bug

--- client/src/components/kulttuurituoteView/Teos/TeosNaytto.jsx
+++ client/src/components/kulttuurituoteView/Teos/TeosNaytto.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Segment, Grid, Loader } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
-import AktiivinenTeos from './AktiivinenTeos'
-import HakuKomponentti from '../../HakuKomponentti'
-import Kirjainhakukomponentti from '../../Kirjainhakukomponentti'
-import UserContext from '../../../context/userContext'
-import { suodata, valitseHakumetodi } from '../../../utilities/hakutoiminnot'
-import { getKulttuurituotteet, deleteData, putData } from '../../../api/api'
+import AktiivinenTeos from './AktiivinenTeos';
+import HakuKomponentti from '../../HakuKomponentti';
+import Kirjainhakukomponentti from '../../Kirjainhakukomponentti';
+import UserContext from '../../../context/userContext';
+import {
+  suodata,
+  suodataKulttuurituotteet,
+  valitseHakumetodi,
+} from '../../../utilities/hakutoiminnot';
+import { getKulttuurituotteet, deleteData, putData } from '../../../api/api';
 
 import './TeosNaytto.css';
 
@@ -16,209 +20,223 @@ const TeosNaytto = ({ className }) => {
   // eslint-disable-next-line no-unused-vars
   const [aktiivinenTeos, setAktiivinenTeos] = useState(undefined);
   const [teokset, setTeokset] = useState([]);
-  const [ladataan, setLadataan] = useState(true); 
+  const [ladataan, setLadataan] = useState(true);
 
-  const [suodatusoptio, setSuodatusoptio] = useState('')
-  const [hakutermi, setHakutermi] = useState('')
+  const [suodatusoptio, setSuodatusoptio] = useState('');
+  const [hakutermi, setHakutermi] = useState('');
   const [suodatusPaalla, setSuodatusPaalla] = useState(false);
-  const sessioData = useContext(UserContext)
+  const sessioData = useContext(UserContext);
 
-  const suodataTeosnimella = suodata ('nimi')
-  const suodataLajityypilla = suodata ('lajityyppi')
-  const suodataPaikkakunnalla = suodata ('teos_paikkakunta')
-  const suodataMaalla = suodata ('teos_maa')
-  const suodataAsiasanalla = suodata ('asiasana')
+  const suodataTeosnimella = suodata('nimi');
+  const suodataLajityypilla = suodata('lajityyppi');
+  const suodataPaikkakunnalla = suodata('teos_paikkakunta');
+  const suodataMaalla = suodata('teos_maa');
+  const suodataAsiasanalla = suodataKulttuurituotteet('asiasana');
 
   useEffect(() => {
-    let mounted = true
+    let mounted = true;
     if (mounted) {
-      setLadataan(true)
-      haeTeokset().then(setLadataan(false))  
+      setLadataan(true);
+      haeTeokset().then(setLadataan(false));
     }
 
-    return () => mounted = false
-  }, [] )
+    return () => (mounted = false);
+  }, []);
 
   const haeTeokset = async () => {
-    const result = await getKulttuurituotteet()
+    const result = await getKulttuurituotteet();
     if (result.status === 'success') {
-      const teoksetKoonti = result.data.henkilot.reduceRight((kooste, tekija) => 
-        kooste.concat(tekija.teokset.map(teos => (
-          {
-            id: teos.id,
-            nimi: teos.nimi,
-            lajityyppi: teos.lajityyppi,
-            teos_maa: teos.teos_maa,
-            teos_paikkakunta: teos.teos_paikkakunta,
-            asiasana: teos.asiasana,
-            valmis: teos.valmis,
-            viesti: teos.viesti,
-            teos_tekija: {
-              id: tekija.id,
-              etunimi: tekija.etunimi,
-              sukunimi: tekija.sukunimi,
-              ammattinimike: tekija.ammattinimike,
-              maa: tekija.maa,
-              paikkakunta: tekija.paikkakunta
-            }
-          }
-      ))), [])
-      teoksetKoonti.sort((a,b) => a['nimi'] < b['nimi'] ? -1 : 1 )
-      setTeokset(teoksetKoonti)
+      const teoksetKoonti = result.data.henkilot.reduceRight(
+        (kooste, tekija) =>
+          kooste.concat(
+            tekija.teokset.map(teos => ({
+              id: teos.id,
+              nimi: teos.nimi,
+              lajityyppi: teos.lajityyppi,
+              teos_maa: teos.teos_maa,
+              teos_paikkakunta: teos.teos_paikkakunta,
+              asiasana: teos.asiasana,
+              valmis: teos.valmis,
+              viesti: teos.viesti,
+              teos_tekija: {
+                id: tekija.id,
+                etunimi: tekija.etunimi,
+                sukunimi: tekija.sukunimi,
+                ammattinimike: tekija.ammattinimike,
+                maa: tekija.maa,
+                paikkakunta: tekija.paikkakunta,
+              },
+            })),
+          ),
+        [],
+      );
+      teoksetKoonti.sort((a, b) => (a['nimi'] < b['nimi'] ? -1 : 1));
+      setTeokset(teoksetKoonti);
     } else {
       // TODO FAILURE
     }
-  }
+  };
 
   const suodatusMuutettu = (suodatusBool, optio, hakutermi) => {
-    setAktiivinenTeos(undefined)
-    setSuodatusPaalla(suodatusBool)
-    setSuodatusoptio(optio)
-    setHakutermi(hakutermi)
-  }
+    setAktiivinenTeos(undefined);
+    setSuodatusPaalla(suodatusBool);
+    setSuodatusoptio(optio);
+    setHakutermi(hakutermi);
+  };
 
   const naytaTeokset = () => {
-    let suodatetutTeokset = []
-    let { hakutermiTrim, predikaatti } = valitseHakumetodi (hakutermi)
+    let suodatetutTeokset = [];
+    let { hakutermiTrim, predikaatti } = valitseHakumetodi(hakutermi);
     if (suodatusPaalla) {
       switch (suodatusoptio) {
         case 'kirjainhaku':
         case 'teosnimi':
-          suodatetutTeokset = suodataTeosnimella (hakutermiTrim) (predikaatti) (teokset)
+          suodatetutTeokset = suodataTeosnimella(hakutermiTrim)(predikaatti)(
+            teokset,
+          );
           break;
         case 'teoslaji':
-          suodatetutTeokset = suodataLajityypilla (hakutermiTrim) (predikaatti) (teokset)
+          suodatetutTeokset = suodataLajityypilla(hakutermiTrim)(predikaatti)(
+            teokset,
+          );
           break;
         case 'teosmaa':
-          suodatetutTeokset = suodataMaalla (hakutermiTrim) (predikaatti) (teokset)
+          suodatetutTeokset = suodataMaalla(hakutermiTrim)(predikaatti)(
+            teokset,
+          );
           break;
         case 'teospaikkak':
-          suodatetutTeokset = suodataPaikkakunnalla (hakutermiTrim) (predikaatti) (teokset)
+          suodatetutTeokset = suodataPaikkakunnalla(hakutermiTrim)(predikaatti)(
+            teokset,
+          );
           break;
         case 'asiasana':
-          suodatetutTeokset = suodataAsiasanalla (hakutermiTrim) (predikaatti) (teokset)
+          suodatetutTeokset = suodataAsiasanalla(hakutermiTrim)(predikaatti)(
+            teokset,
+          );
           break;
         default:
-          suodatetutTeokset = teokset
+          suodatetutTeokset = teokset;
           break;
       }
     } else {
-      suodatetutTeokset = teokset
+      suodatetutTeokset = teokset;
     }
 
-    // jos käyttäjä ei ole kirjautuneena, poistetaan hakusanoista ne joissa ei ole 
+    // jos käyttäjä ei ole kirjautuneena, poistetaan hakusanoista ne joissa ei ole
     // yhtään ilmentymää jossa valmis = true
     if (!sessioData.token) {
-      suodatetutTeokset = suodatetutTeokset.filter(teos => teos['valmis'] === true)
+      suodatetutTeokset = suodatetutTeokset.filter(
+        teos => teos['valmis'] === true,
+      );
     }
 
     return (
       <>
         {suodatetutTeokset.map((item, index) => {
           return (
-          <Grid.Row key={index}>
-            <div className="menuitem" onClick={()=>setAktiivinenTeos(item)}>
-              <b>{item.nimi}</b> {"(" + item.lajityyppi + ")"}
-            </div>
-          </Grid.Row>
-          )
+            <Grid.Row key={index}>
+              <div className="menuitem" onClick={() => setAktiivinenTeos(item)}>
+                <b>{item.nimi}</b> {'(' + item.lajityyppi + ')'}
+              </div>
+            </Grid.Row>
+          );
         })}
-      </>  
-    )
-  }
+      </>
+    );
+  };
   const KULTTUURITUOTTEET_HAKUOPTIOT = [
-    { key: "teosnimi", text: "Teoksen nimi", value: "teosnimi" },
-    { key: "teoslaji", text: "Teoksen laji", value: "teoslaji" },
-    { key: "teospaikkak", text: "Paikkakunta", value: "teospaikkak" },
-    { key: "teosmaa", text: "Maa", value: "teosmaa" },
-    { key: "asiasana", text: "Asiasana", value: "asiasana" },
-  ]
+    { key: 'teosnimi', text: 'Teoksen nimi', value: 'teosnimi' },
+    { key: 'teoslaji', text: 'Teoksen laji', value: 'teoslaji' },
+    { key: 'teospaikkak', text: 'Paikkakunta', value: 'teospaikkak' },
+    { key: 'teosmaa', text: 'Maa', value: 'teosmaa' },
+    { key: 'asiasana', text: 'Asiasana', value: 'asiasana' },
+  ];
 
-  const KULTTUURITUOTTEET_DEFAULT = "teosnimi"
+  const KULTTUURITUOTTEET_DEFAULT = 'teosnimi';
 
-  const poistoHandler = poistettava => teos_id => (hlo_id) => {
+  const poistoHandler = poistettava => teos_id => hlo_id => {
     switch (poistettava) {
       case 'teos':
-        deleteData('/api/kulttuuriteos/', teos_id, sessioData.token)
-        .then(result => {
-          if (result.status === 'success') {
-           haeTeokset().then(() => {
-              setAktiivinenTeos(null)
-            })
-          }
-        })
+        deleteData('/api/kulttuuriteos/', teos_id, sessioData.token).then(
+          result => {
+            if (result.status === 'success') {
+              haeTeokset().then(() => {
+                setAktiivinenTeos(null);
+              });
+            }
+          },
+        );
         break;
       case 'tekija':
-        deleteData('/api/henkilo/', hlo_id, sessioData.token)
-        .then(result => {
+        deleteData('/api/henkilo/', hlo_id, sessioData.token).then(result => {
           if (result.status === 'success') {
             haeTeokset().then(() => {
-              setAktiivinenTeos(null)
-            })
+              setAktiivinenTeos(null);
+            });
           }
-        })
+        });
         break;
       default:
         break;
     }
-  }
+  };
 
   const updateHandler = muutettava => uusiData => {
-    const { tyyppi, id } = muutettava
+    const { tyyppi, id } = muutettava;
     switch (tyyppi) {
       case 'teos':
-        putData('/api/kulttuuriteos/', uusiData, id, sessioData.token)
-        .then(result => {
-          if (result.status === 'success') {
-            haeTeokset().then(() => {
-              setAktiivinenTeos(null)
-            })
-          }
-        })
+        putData('/api/kulttuuriteos/', uusiData, id, sessioData.token).then(
+          result => {
+            if (result.status === 'success') {
+              haeTeokset().then(() => {
+                setAktiivinenTeos(null);
+              });
+            }
+          },
+        );
         break;
       default:
         break;
     }
-  }
+  };
 
-  const Latauskomponentti = () => (
-    <Loader active>Ladataan tietoja</Loader>
-  )
+  const Latauskomponentti = () => <Loader active>Ladataan tietoja</Loader>;
 
   return (
     <>
-    { ladataan
-    ? Latauskomponentti()
-    : (
-    <div className={className}>
-      <Segment basic>
-        <Kirjainhakukomponentti
-          suodatusMuutettu={suodatusMuutettu}
-        />
-        <HakuKomponentti
-          hakuOptiot={KULTTUURITUOTTEET_HAKUOPTIOT}
-          defaultHaku={KULTTUURITUOTTEET_DEFAULT}
-          suodatusMuutettu={suodatusMuutettu} />
-        <Grid columns={16}>
-          <Grid.Row>
-            <Grid.Column width={6} textAlign="left">
-                {naytaTeokset()}
-            </Grid.Column>
-            <Grid.Column width={10}>
-              {aktiivinenTeos
-              ? <AktiivinenTeos
-                  aktiivinenTeos={aktiivinenTeos}
-                  suodatus={{suodatusPaalla, suodatusoptio, hakutermi}}
-                  poistoHandler={poistoHandler}
-                  updateHandler={updateHandler}
-                />
-              : null}
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-    </div>)}
-  </>
+      {ladataan ? (
+        Latauskomponentti()
+      ) : (
+        <div className={className}>
+          <Segment basic>
+            <Kirjainhakukomponentti suodatusMuutettu={suodatusMuutettu} />
+            <HakuKomponentti
+              hakuOptiot={KULTTUURITUOTTEET_HAKUOPTIOT}
+              defaultHaku={KULTTUURITUOTTEET_DEFAULT}
+              suodatusMuutettu={suodatusMuutettu}
+            />
+            <Grid columns={16}>
+              <Grid.Row>
+                <Grid.Column width={6} textAlign="left">
+                  {naytaTeokset()}
+                </Grid.Column>
+                <Grid.Column width={10}>
+                  {aktiivinenTeos ? (
+                    <AktiivinenTeos
+                      aktiivinenTeos={aktiivinenTeos}
+                      suodatus={{ suodatusPaalla, suodatusoptio, hakutermi }}
+                      poistoHandler={poistoHandler}
+                      updateHandler={updateHandler}
+                    />
+                  ) : null}
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+        </div>
+      )}
+    </>
   );
 };
 
--- client/src/utilities/hakutoiminnot.js
+++ client/src/utilities/hakutoiminnot.js
@@ -1,58 +1,71 @@
 // PREDIKAATTIFUNKTIOT HAKUA VARTEN
 
 // isArray check koska kohde voi olla string tai [string]
-const sisaltaa = hakutermi => kohde => 
+const sisaltaa = hakutermi => kohde =>
   //(Array.isArray(kohde) ? kohde[0] : kohde ).toUpperCase().indexOf( hakutermi.toUpperCase() ) >= 0
-  (Array.isArray(kohde) ? kohde[0] : kohde ).toUpperCase().includes( hakutermi.toUpperCase())
+  (Array.isArray(kohde) ? kohde[0] : kohde)
+    .toUpperCase()
+    .includes(hakutermi.toUpperCase());
 
-const alkaa = hakutermi => kohde => 
+const alkaa = hakutermi => kohde =>
   //(Array.isArray(kohde) ? kohde[0] : kohde).toUpperCase().indexOf( hakutermi.toUpperCase() ) === 0
-  (Array.isArray(kohde) ? kohde[0] : kohde).toUpperCase().startsWith( hakutermi.toUpperCase());
+  (Array.isArray(kohde) ? kohde[0] : kohde)
+    .toUpperCase()
+    .startsWith(hakutermi.toUpperCase());
 
 const paattyy = hakutermi => kohde => {
-  return (Array.isArray(kohde) ? kohde[0] : kohde).toUpperCase().endsWith( hakutermi.toUpperCase())
-}
-  
+  return (Array.isArray(kohde) ? kohde[0] : kohde)
+    .toUpperCase()
+    .endsWith(hakutermi.toUpperCase());
+};
+
 // SUODATUSFUNKTIO
 
 export const suodata = attribuutti => hakutermi => suodatin => taulukko => {
+  console.log(attribuutti);
   return taulukko.filter(objekti => {
-    return suodatin (hakutermi) (objekti[attribuutti])
-  })
-}
+    return suodatin(hakutermi)(objekti[attribuutti]);
+  });
+};
 
+export const suodataKokoelma = kokoelma => attribuutti => hakutermi => suodatin => taulukko =>
+  taulukko.filter(objekti =>
+    objekti[kokoelma].some(ilmentyma =>
+      ilmentyma[attribuutti].some(sana => suodatin(hakutermi)(sana)),
+    ),
+  );
 
-export const suodataKokoelma = kokoelma => attribuutti => hakutermi => suodatin => taulukko => 
-  taulukko.filter(objekti => objekti[kokoelma]
-    .some(ilmentyma => ilmentyma[attribuutti].some(sana => suodatin (hakutermi) (sana)))
-  )
+export const suodataKulttuurituotteet = attribuutti => hakutermi => suodatin => taulukko =>
+  taulukko.filter(objekti =>
+    objekti[attribuutti].some(sana => suodatin(hakutermi)(sana)),
+  );
 
-  // export const suodataKokoelma = kokoelma => attribuutti => hakutermi => suodatin => taulukko =>
-  // taulukko.filter(objekti => objekti[kokoelma]
-  //   .some(ilmentyma => suodatin (hakutermi) (ilmentyma[attribuutti][0]))
-  // )
+// export const suodataKokoelma = kokoelma => attribuutti => hakutermi => suodatin => taulukko =>
+// taulukko.filter(objekti => objekti[kokoelma]
+//   .some(ilmentyma => suodatin (hakutermi) (ilmentyma[attribuutti][0]))
+// )
 
 // HAKUSANAN TRIMMAUS JA PREDIKAATIN VALINTA
 export const valitseHakumetodi = hakusana => {
   if (hakusana[0] === '*' && hakusana[hakusana.length - 1] === '*') {
     return {
       hakutermiTrim: hakusana.slice(1, hakusana.length - 1),
-      predikaatti: sisaltaa
-    }
+      predikaatti: sisaltaa,
+    };
   } else if (hakusana[0] === '*') {
     return {
       hakutermiTrim: hakusana.slice(1),
-      predikaatti: paattyy
-    }
+      predikaatti: paattyy,
+    };
   } else if (hakusana[hakusana.length - 1] === '*') {
     return {
       hakutermiTrim: hakusana.slice(0, hakusana.length - 1),
-      predikaatti: alkaa
-    }
+      predikaatti: alkaa,
+    };
   } else {
     return {
       hakutermiTrim: hakusana,
-      predikaatti: alkaa
-    }
+      predikaatti: alkaa,
+    };
   }
-}
+};
