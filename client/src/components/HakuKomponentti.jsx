e8c239d21447dc1fc1f19626c2fb60d357bd5b12
tuomashen <51713168+tuomashen@users.noreply.github.com>
2021-03-03 05:42:15 +0200

    UI vuosihaulle aloitettu.

--- client/src/components/HakuKomponentti.jsx
+++ client/src/components/HakuKomponentti.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Dropdown, Segment, Input, Button } from 'semantic-ui-react'
+import { Dropdown, Segment, Input, Button, Checkbox } from 'semantic-ui-react'
 
 import './HakuKomponentti.css'
 
@@ -9,6 +9,9 @@ const Hakukomponentti = ({ suodatusMuutettu, hakuOptiot, defaultHaku }) => {
   const [hakutermi, setHakutermi] = useState('')
   const [hakutermiValittu, setHakutermiValittu] = useState('')
   const [valittuSuodatusOptio, setValittuSuodatusOptio] = useState(defaultHaku)
+  const [alkuvuosi, setAlkuvuosi] = useState('1900')
+  const [loppuvuosi, setLoppuvuosi] = useState('2021')
+  const [lisaOptiot, setLisaOptiot] = useState(false)
 
   const hakuoptioMuutettu = (e, data) => {
     setValittuSuodatusOptio(data.value)
@@ -23,6 +26,7 @@ const Hakukomponentti = ({ suodatusMuutettu, hakuOptiot, defaultHaku }) => {
     setSuodatusPaalla(true)
     setHakutermiValittu(hakutermi)
     setHakutermi('')
+    //console.log(alkuvuosi + " - " + loppuvuosi)
   }
 
   const resetoiHaku = () => {
@@ -36,6 +40,18 @@ const Hakukomponentti = ({ suodatusMuutettu, hakuOptiot, defaultHaku }) => {
     if (e.key === 'Enter') {
       hakuKlikattu()
     }
+  }
+
+  const lisaOptiotMuutettu = () => {
+    setLisaOptiot(!lisaOptiot);
+  }
+
+  const alkuvuosiMuutettu = (e, data) => {
+    setAlkuvuosi(data.value)
+  }
+
+  const loppuvuosiMuutettu = (e, data) => {
+    setLoppuvuosi(data.value)
   }
 
   let hakuKentta;
@@ -57,16 +73,29 @@ const Hakukomponentti = ({ suodatusMuutettu, hakuOptiot, defaultHaku }) => {
         >
         </Dropdown>
         <div>
-          <Input 
-            type="checkbox"
-            className="hakukentta"
-            onChange={hakuTermiMuutettu}
-            onKeyPress={nappainPainettu}
-            style={{ marginTop: '0rem', }}
-            value={hakutermi}
-          /> <label>Vuosihaku</label>
+          <Checkbox toggle 
+            label="Suodata"
+            onChange={lisaOptiotMuutettu}
+            />
+          { lisaOptiot ?
+            <div>
+            Alku: 
+            <Input 
+              className="hakukentta"
+              style={{ marginTop: '0rem', }}
+              value={alkuvuosi}
+              onChange={alkuvuosiMuutettu}
+            />
+            Loppu: 
+            <Input 
+              className="hakukentta"
+              style={{ marginTop: '0rem', }}
+              value={loppuvuosi}
+              onChange={loppuvuosiMuutettu}
+            />
+            </div> : null
+          }
         </div>
-
       </Segment>
       ) 
   } else {
