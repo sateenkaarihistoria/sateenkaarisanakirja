8ce6ce17f312ee4c9c73a0c791466ea95f06f410
Sauli Nevalainen <nevalainen.sauli@gmail.com>
2021-03-09 22:23:01 +0200

    Korjaa useampien paikkojen ilmentyminen henkilöillä

--- models/henkilo.js
+++ models/henkilo.js
@@ -13,16 +13,16 @@ WHERE asiasana.id = asiasana_id AND teos.id = teos_id
     'viesti', teos.viesti,
          'teos_maa', maa,
          'teos_paikkakunta', paikkakunta,
-    'asiasana', asiasanat
+    'asiasanat', asiasanat
                       ) js
     FROM teos, y, tapahtuu_teos, sijainti
     WHERE teos.id = y.id AND sijainti_id = sijainti.id AND teos_id = teos.id)
 
-SELECT henkilo.id, etunimi, sukunimi, ammattinimike, maa, paikkakunta, json_agg(t.js) teokset
+SELECT henkilo.id, etunimi, sukunimi, ammattinimike, MAX(maa), MAX(paikkakunta), json_agg(t.js) teokset
 FROM henkilo, sijainti, toimii_henkilo, t, tekee
 WHERE sijainti_id = sijainti.id AND henkilo.id = toimii_henkilo.henkilo_id AND t.id = tekee.teos_id and tekee.henkilo_id = henkilo.id
-GROUP BY henkilo.id, etunimi, sukunimi, ammattinimike, maa, paikkakunta
-ORDER BY sukunimi ASC;`;
+GROUP BY henkilo.id, etunimi, sukunimi, ammattinimike
+ORDER BY sukunimi;`;
 
 exports.haeHenkilot = async () => {
   try {
