2514d44e2e934117f77ef056edc42d9cb1e9ff85
tuomashen <51713168+tuomashen@users.noreply.github.com>
2021-04-05 00:26:34 +0300

    Teosten tulostus /api/tulostus/teokset

--- controllers/tulostusController.js
+++ controllers/tulostusController.js
@@ -5,12 +5,47 @@ const asiasana = require("./asiasanaController");
 ("use strict");
 
 exports.tulostaSanat = async function (req, res, next) {
-  // const result = "Tulostus. Tähän tulee sanalista";
-  // const result = await hakusanat();
   const result = await hakusanat();
 
   res.set('Content-Type', 'text/html');
   res.status(200).send(result);
+};
+
+exports.tulostaTeokset = async function (req, res, next) {
+	const result = await haeHenkilot();
+
+	var resStr = "<html><body>";
+
+	for (var i = 0; i < result.henkilot.length; i++) {
+		resStr += "<div>";
+		resStr += "<b>" + result.henkilot[i].etunimi + " " + result.henkilot[i].sukunimi + "</b>";
+		resStr += " (" + result.henkilot[i].ammattinimike +")<br>";
+		resStr += " (" + result.henkilot[i].paikkakunta + ", " + result.henkilot[i].maa +")<br>";
+
+		resStr += "<div><b>Teokset:</b><br>";
+		for (var j = 0; j < result.henkilot[i].teokset.length; j++) {
+			resStr += "<div>";
+			resStr +=  "<i>" + result.henkilot[i].teokset[j].nimi + "</i>" + "(" + result.henkilot[i].teokset[j].lajityyppi + ")<br>";
+			resStr += result.henkilot[i].teokset[j].teos_paikkakunta + ", " + result.henkilot[i].teokset[j].teos_maa + "<br>";
+			resStr += result.henkilot[i].teokset[j].viesti + "<br>";
+
+			for (var k = 0; k < result.henkilot[i].teokset[j].asiasana.length; k++) {
+				resStr += result.henkilot[i].teokset[j].asiasana[k];
+				if (k < result.henkilot[i].teokset[j].asiasana.length-1) {
+					resStr += ", "
+				}
+			}
+			resStr += "</div>";
+		}
+		resStr += "</div>"
+
+		resStr += "</div><hr>";
+	}
+
+	resStr += "</body></html>";
+
+	res.set('Content-Type', 'text/html');
+  	res.status(200).send(resStr);
 };
 
 const hakusanat = async () => {
@@ -67,7 +102,7 @@ const hakusanat = async () => {
   	res += "</div><hr>";
   }
 
-  res += "</html></body>";
+  res += "</body></html>";
 
   return res;
 };
@@ -206,4 +241,139 @@ const kaikkiSanat = async (hakusanat) => {
     palautus.sanat.push(hakuSana);
   }
   return palautus;
+};
+
+const haeHenkilot = async () => {
+  // Etsitään kaikki henkilot
+  const henkilot = await new Promise((resolve, reject) =>
+    connection.query("SELECT * FROM henkilo", (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results.rows);
+      }
+    })
+  );
+
+  const res = await haeTiedot(henkilot);
+  return res;
+};
+
+const haeTiedot = async (henkilot) => {
+  // palautettava objekti
+  const palautus = { henkilot: [] };
+
+  for (let j = 0; j < henkilot.length; j++) {
+    const henkilo = henkilot[j];
+    // Etsitään henkilon teokset
+    const kysely3 =
+      "SELECT teos.id, teos.nimi, teos.lajityyppi, teos.valmis, teos.viesti FROM teos, tekee, henkilo WHERE henkilo_id = $1 AND henkilo.id = henkilo_id AND teos.id = teos_id";
+    const teokset = await new Promise((resolve, reject) =>
+      connection.query(kysely3, [henkilo.id], (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          const tuotteet = results.rows;
+          // Etsitään teoksen asiasanat
+          const kysely =
+            "SELECT DISTINCT (kuvaus) FROM teos, asiasana, sisaltaa WHERE asiasana.id = asiasana_id AND teos.id = teos_id AND teos_id = $1";
+          for (let i = 0; i < results.rows.length; i++) {
+            const aSanat = [];
+            // Etsitaan teoksen sijainti
+            connection.query(
+              "SELECT maa, paikkakunta FROM sijainti, tapahtuu_teos, teos WHERE sijainti.id = sijainti_id AND teos.id = teos_id AND teos.id = $1",
+              [results.rows[i].id],
+              (error, results2) => {
+                if (results2.rows.length > 0) {
+                  // Lisataan teoksen sijainti teokseen
+                  tuotteet[i].teos_maa = results2.rows[0].maa;
+                  tuotteet[i].teos_paikkakunta = results2.rows[0].paikkakunta;
+                }
+                // Teoksen asiasanat
+                connection.query(
+                  kysely,
+                  [results.rows[i].id],
+                  (error, results) => {
+                    if (error) {
+                      return error;
+                    }
+                    // Jos ei asiasanoja palauta teokset
+                    if (results.rows.length === 0) {
+                      resolve(tuotteet);
+                    } else {
+                      // Lisää teokseen asiasanoja
+                      for (let k = 0; k < results.rows.length; k++) {
+                        aSanat.push(results.rows[k].kuvaus);
+                        // Jos viimeinen kierros
+                        if (results.rows.length - k === 1) {
+                          // Lisataan teokseen asiasanojen lista
+                          tuotteet[i].asiasana = aSanat;
+                          // Jos kaikki teokset tarkistettu palauta teokset
+                          if (tuotteet.length - i === 1) {
+                            resolve(tuotteet);
+                          }
+                        }
+                      }
+                    }
+                  }
+                );
+              }
+            );
+          }
+          // Jos Henkilolla ei teoksia
+          if (results.rows.length === 0) {
+            resolve([]);
+          }
+        }
+      })
+    );
+
+    // Etsitään henkilon sijainti
+    const kysely =
+      "SELECT maa, paikkakunta FROM sijainti, toimii_henkilo, henkilo WHERE henkilo_id = $1 AND sijainti.id = sijainti_id AND henkilo.id = henkilo_id";
+    const sijainti = await new Promise((resolve, reject) =>
+      connection.query(kysely, [henkilo.id], (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results.rows);
+        }
+      })
+    );
+
+    // jos henkilolla on sijainti
+    if (sijainti.length > 0) {
+      // Lisataan henkiloon maa
+      henkilo.maa = sijainti[0].maa;
+
+      // Lisataan henkiloon paikkakunta
+      henkilo.paikkakunta = sijainti[0].paikkakunta;
+    }
+    // Lisataan henkiloon teokset
+    henkilo.teokset = teokset;
+
+    // Lisätään henkilo listaan
+    palautus.henkilot.push(henkilo);
+  }
+  return palautus;
+};
+
+// Palauttaa henkilon
+exports.returnHenkilo = function (req, res, next) {
+  const id = parseInt(req.params.id);
+  connection.query(
+    "SELECT * FROM henkilo WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        next(error);
+      } else if (results.rows[0] == null) {
+        res
+          .status(404)
+          .json({ error: "Henkilo with given id does not exist!" });
+      } else {
+        res.status(200).json(results.rows[0]);
+      }
+    }
+  );
 };
--- routes/tulostus/root.js
+++ routes/tulostus/root.js
@@ -1,15 +1,10 @@
 const { Router } = require("express");
 
 const tulostusController = require("../../controllers/tulostusController");
-/*const asiasanaController = require("../../controllers/asiasanaController");
-
-const validator = require("../../controllers/validointiController.js");
-const hakusana = require("../../controllers/hakusana");
-*/
 
 const api = Router();
 
 api.get("/", tulostusController.tulostaSanat);
-//api.get("/", validator.vuosiluvut, hakusana.hakuKaikki);
+api.get("/teokset", tulostusController.tulostaTeokset);
 
 module.exports = api;
