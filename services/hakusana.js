786b7ae53d12747636273c6db39a479527f25a62
tuomashen <51713168+tuomashen@users.noreply.github.com>
2021-04-29 18:41:06 +0300

    haeLista korjaus

--- services/hakusana.js
+++ services/hakusana.js
@@ -1,8 +1,8 @@
 const hakusana = require('../models/hakusana.js');
 
 exports.haeLista = async (alku = 0, loppu = 9999) => {
-  const alkuPvm = new Date(alku, 1, 1);
-  const loppuPvm = new Date(loppu, 12, 31);
+  const alkuPvm = new Date(alku, 0, 1);
+  const loppuPvm = new Date(loppu, 11, 31);
   const paluu = await hakusana.haeHakuSanat(alkuPvm, loppuPvm);
   return paluu;
 }
